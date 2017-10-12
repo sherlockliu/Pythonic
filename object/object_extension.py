f616906f6619094940354683c38c41522c57aff1
sherlockliu <hancock.liu@gmail.com>
2017-10-12 11:45:26 +0800

    add folders

---
+++ object/object_extension.py
@@ -0,0 +1,8 @@
+# ！/usr/bin/python
+
+class ObjectExtension:
+    def contains_attr(self, o: object, path: str) -> bool:
+        if path is not None and o is not None:
+            pass
+        else:
+            return
